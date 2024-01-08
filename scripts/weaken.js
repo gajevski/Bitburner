5eaef86a7070e0e5dbdd756319349c20ce69a070
gajevski <mikolajxgajewski@gmail.com>
2024-01-08 22:00:40 +0100

    add script to weaken security level

---
+++ scripts/weaken.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const LIMIT = 10;
+    const SERVER_NAME = 'joesguns';
+    let x = 0;
+  
+    while (x <= LIMIT) {
+      await ns.weaken(SERVER_NAME);
+      x++;
+    }
+  }
+  
