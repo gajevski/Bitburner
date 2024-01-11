d453c3b80a29a2cc198c61c420f4579b8302a89a
gajevski <mikolajxgajewski@gmail.com>
2024-01-11 15:21:10 +0100

    refactor hack script

--- scripts/hack.js
+++ scripts/hack.js
@@ -1,6 +1,21 @@
-/** @param {NS} ns */
+/** @param {NS} ns **/
 export async function main(ns) {
+    const target = ns.args[0];
     while (true) {
-        await ns.hack(ns.args[0]);
+        const securityThreshold = ns.getServerMinSecurityLevel(target) + 5;
+        const moneyThreshold = ns.getServerMaxMoney(target) * 0.75;
+
+        if (ns.getServerSecurityLevel(target) > securityThreshold) {
+            ns.toast("Weakening " + target + " due to high security level.");
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
+            ns.print("Growing " + target + " due to low available money.");
+            await ns.grow(target);
+        } else {
+            ns.print("Hacking " + target + ".");
+            const hackedAmount = await ns.hack(target);
+            const formattedAmount = Number(hackedAmount.toFixed(2)).toLocaleString('en-US', { minimumFractionDigits: 2 });
+            ns.toast(`Hacked \$${formattedAmount} from ${target} through ${ns.args[0]}.`, "success", 5000);
+        }
     }
 }
